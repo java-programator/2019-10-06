6cbce1e8fce20bfa82c9cbf9b806d5c28fc10d16
tw <tomaszwiszynski@gmail.com>
2019-10-06 17:59:34 +0200

    talia kart added

--- src/pl/altkom/karty/TasowanieKart.java
+++ src/pl/altkom/karty/TasowanieKart.java
@@ -1,6 +1,6 @@
 package pl.altkom.karty;
 
-import java.util.Arrays;
+import java.util.*;
 
 enum KolorKarty {
     KIER, KARO, PIK, TREFL;
@@ -18,16 +18,52 @@ class Karta {
         this.kolor = kolor;
         this.figura = figura;
     }
+
+    @Override
+    public String toString() {
+        return "Karta{" +
+                "kolor=" + kolor +
+                ", figura=" + figura +
+                '}';
+    }
 }
 
 public class TasowanieKart {
     public static void main(String[] args) {
-        KolorKarty k = KolorKarty.KARO;
-        System.out.println(k.name());
-        System.out.println(k.ordinal());
-        System.out.println(k.compareTo(KolorKarty.PIK));
-        System.out.println(k.compareTo(KolorKarty.KIER));
-        System.out.println(Arrays.toString(KolorKarty.values()));
-        System.out.println(KolorKarty.valueOf("PIK"));
+        List<Karta> talia = new ArrayList<>();
+        for (KolorKarty kolor : KolorKarty.values()){
+            for (FiguraKarty figura : FiguraKarty.values()){
+                Karta karta = new Karta(kolor, figura);
+                talia.add(karta);
+            }
+        }
+
+        Collections.shuffle(talia);
+
+        for (Karta karta: talia) {
+            System.out.println(karta);
+        }
+
+        List<String> players = Arrays.asList("Janek", "Franek", "Romek", "Tomek");
+
+        Map<String, Set<Karta>> rozdanie = new HashMap<>();
+        for (String player : players){
+            rozdanie.put(player, new TreeSet<>());
+        }
+
+
+
+
+
+
+
+
+//        KolorKarty k = KolorKarty.KARO;
+//        System.out.println(k.name());
+//        System.out.println(k.ordinal());
+//        System.out.println(k.compareTo(KolorKarty.PIK));
+//        System.out.println(k.compareTo(KolorKarty.KIER));
+//        System.out.println(Arrays.toString(KolorKarty.values()));
+//        System.out.println(KolorKarty.valueOf("PIK"));
     }
 }
